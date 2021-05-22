67529ca7d29644e3e38dd4fb2b1f76db4c3999f9
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-05-22 23:13:34 +0700

    chore: add PWA capabilities

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   images: {
     domains: ["raw.githubusercontent.com"],
   },
-};
+  pwa: {
+    disable: process.env.NODE_ENV === "development",
+    dest: "public",
+    register: true,
+  },
+  future: {
+    webpack5: true
+  }
+});
