6795135a201b5118e147601aec2a4f4ced45f323
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-05-23 15:37:10 +0700

    refactor(pokemon): separate catch button and modals from detail wrapper

--- src/components/pokemon/detail/DetailWrapper.tsx
+++ src/components/pokemon/detail/DetailWrapper.tsx
@@ -1,5 +1,3 @@
-import { Button } from "@chakra-ui/button";
-import { useDisclosure } from "@chakra-ui/hooks";
 import {
   Badge,
   Box,
@@ -11,22 +9,10 @@ import {
   WrapItem,
 } from "@chakra-ui/layout";
 import { Skeleton } from "@chakra-ui/skeleton";
-import { useToast } from "@chakra-ui/toast";
-import { FormikErrors, useFormik } from "formik";
 import Image from "next/image";
-import { useRouter } from "next/router";
-import { useContext } from "react";
 
+import CatchWrapper from "./CatchWrapper";
 import AccordionWrapper from "components/ui/accordion";
-import ModalWrapper from "components/ui/ModalWrapper";
-import InputWrapper from "components/ui/InputWrapper";
-import MotionBox from "components/motion/Box";
-
-import {
-  CaughtPokemonContext,
-  CaughtPokemons,
-} from "components/provider/CaughtPokemonProvider";
-import { rand50 } from "util/rand50";
 
 import { PokemonDetail } from "./query";
 
@@ -37,98 +23,7 @@ type DetailWrapperProps = {
   isLoading?: boolean;
 };
 
-type CapturedPokemonInput = {
-  nickName: string;
-};
-
 const DetailWrapper = ({ pokemon, isLoading }: DetailWrapperProps) => {
-  const { pokemons, updateCaughtPokemons } = useContext(CaughtPokemonContext);
-  const router = useRouter();
-  const { values, dirty, errors, setFieldValue, handleChange, handleSubmit } =
-    useFormik<CapturedPokemonInput>({
-      initialValues: {
-        nickName: "",
-      },
-      validate: (formValue) => {
-        const formErrors: FormikErrors<CapturedPokemonInput> = {};
-
-        if (formValue.nickName === "") {
-          formErrors.nickName = "Nickname must be filled.";
-        }
-
-        if (
-          pokemons?.[pokemon.name]?.nickNames
-            ?.map((nickName) => nickName.toLowerCase())
-            .includes(formValue.nickName.toLowerCase())
-        ) {
-          formErrors.nickName =
-            "You already use this nickname for this pokemon. Type another nickname.";
-        }
-
-        return formErrors;
-      },
-      onSubmit: (formValue) => {
-        const tempPokemons = { ...pokemons };
-        const tempNickNames = tempPokemons[pokemon.name]?.nickNames ?? [];
-        const updateStoredPokemons: CaughtPokemons = Object.assign(
-          tempPokemons,
-          {
-            [pokemon.name]: {
-              image: pokemon.sprites.front_default,
-              nickNames: [...tempNickNames, formValue.nickName],
-            },
-          }
-        );
-
-        updateCaughtPokemons(updateStoredPokemons);
-        toast.closeAll();
-        toast({
-          title: `saved as ${formValue.nickName}`,
-          status: "success",
-        });
-        setFieldValue("nickName", "");
-        onCloseSuccessModal();
-        router.push("/pokedex/owned");
-      },
-    });
-
-  const toast = useToast();
-  const {
-    isOpen: isCaptureModalOpen,
-    onOpen: onOpenCaptureModal,
-    onClose: onCloseCaptureModal,
-  } = useDisclosure();
-  const {
-    isOpen: isSuccessModalOpen,
-    onOpen: onOpenSuccessModal,
-    onClose: onCloseSuccessModal,
-  } = useDisclosure();
-
-  const handleCapturePokemon = () => {
-    onOpenCaptureModal();
-    const success = rand50();
-
-    setTimeout(() => {
-      onCloseCaptureModal();
-      toast.closeAll();
-
-      if (success) {
-        toast({
-          title: `${pokemon.name} captured!`,
-          status: "success",
-          isClosable: true,
-        });
-        onOpenSuccessModal();
-      } else {
-        toast({
-          title: `The ${pokemon.name} ran away! Try again some time...`,
-          status: "info",
-          isClosable: true,
-        });
-      }
-    }, 2000);
-  };
-
   return (
     <Grid gap={8}>
       <Grid gap={8} templateColumns={["1fr", "1fr", "repeat(2, 1fr)"]}>
@@ -157,14 +52,7 @@ const DetailWrapper = ({ pokemon, isLoading }: DetailWrapperProps) => {
           gridGap={8}
           alignItems="center"
         >
-          <Button
-            onClick={handleCapturePokemon}
-            isFullWidth
-            size="lg"
-            colorScheme="orange"
-          >
-            catch
-          </Button>
+          <CatchWrapper pokemon={pokemon} />
 
           <Flex alignItems="center" gridGap={2} wrap="wrap">
             <Heading width="full" size="sm">
@@ -176,61 +64,6 @@ const DetailWrapper = ({ pokemon, isLoading }: DetailWrapperProps) => {
               ))}
             </Grid>
           </Flex>
-
-          <ModalWrapper
-            isOpen={isCaptureModalOpen}
-            onClose={() => {}}
-            header="Attempting to Capture..."
-            body={
-              <MotionBox
-                marginY={20}
-                initial={{ y: 80 }}
-                animate={{ y: -100, rotate: 180 }}
-                transition={{
-                  repeat: Infinity,
-                  ease: "easeOut",
-                  duration: 0.5,
-                  repeatType: "reverse",
-                }}
-                width={100}
-                marginX="auto"
-              >
-                <Image
-                  src="/pokeball.svg"
-                  layout="responsive"
-                  width="100%"
-                  height="100%"
-                />
-              </MotionBox>
-            }
-          />
-
-          <ModalWrapper
-            isOpen={isSuccessModalOpen}
-            onClose={() => {}}
-            header={`Give your ${pokemon.name} a nickname!`}
-            body={
-              <InputWrapper
-                label="nickname"
-                name="nickName"
-                value={values.nickName}
-                onChange={handleChange}
-                placeholder={`your ${pokemon.name}'s nickname`}
-                isInvalid={!!errors.nickName}
-                helperText={errors.nickName}
-              />
-            }
-            withFooter
-            confirmButton={
-              <Button
-                disabled={!dirty || (dirty && Object.keys(errors).length > 0)}
-                colorScheme="teal"
-                onClick={() => handleSubmit()}
-              >
-                save
-              </Button>
-            }
-          />
         </Skeleton>
       </Grid>
 
